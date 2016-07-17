071ed5a31c6e89deb255d300ac321af986675a76
petya <petrmikheev@ya.ru>
2016-07-17 12:14:44 +0300

    Makefile for bootloader (miksys_soft/ustartup) was added

---
+++ miksys_soft/ustartup/rebuild_startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+../compile.py startup.S startup.bin > tmp
+for func in divide_func set_number_base_func printf_func usb_request_func ; do
+	addr=`grep "$func = " tmp | cut -d ' ' -f 3`
+	echo $func : $addr
+	sed -i "s/#define $func .*$/#define $func $addr/g" ../include/std.H
+done
+rm tmp
